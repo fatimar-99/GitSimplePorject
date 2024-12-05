1cfaf55e485eeaea7265204c90789409c1566d28
Fatima <fatimarq@gmail.com>
2024-12-05 13:25:22 -0600

    first commit

--- src/TestDemo/Test101.java
+++ src/TestDemo/Test101.java
@@ -3,7 +3,13 @@ package TestDemo;
 public class Test101 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something...");
+		else {
+			System.out.println("go to sleep!");
+		}
 
 	}
 
