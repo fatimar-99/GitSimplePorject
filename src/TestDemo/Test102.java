81da31100c67554fe0e18ad99fadca837e30a3ac
Fatima <fatimarq@gmail.com>
2024-12-09 13:20:23 -0600

    committing Test102 to Branch101

---
+++ src/TestDemo/Test102.java
@@ -0,0 +1,16 @@
+package TestDemo;
+
+public class Test102 {
+
+	public static void main(String[] args) {
+		// 
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something...");
+		else {
+			System.out.println("go to sleep!");
+		}
+
+	}
+
+}
